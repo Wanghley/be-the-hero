5e0e4ddc7865b3462e45623598bd0a3d279b7d64
Wanghley Soares Martins <wanghleys@gmail.com>
2020-03-26 23:41:03 -0300

    [UPDATE] bug fix - id gets incidents

--- backend/src/controllers/IncidentController.js
+++ backend/src/controllers/IncidentController.js
@@ -7,11 +7,18 @@ module.exports={
         const [count] = await connection('incidents').count();
 
         const incidents = await connection('incidents')
-            .join('ongs','ong_id','=','incidents.ong_id')
-            .limit(5)
-            .offset((page-1)*5)
-            .select(['incidents.*','ongs.nome',
-                'ongs.email','ongs.whatsapp','ongs.city','ongs.uf']);
+        .join('ongs', 'ongs.id', '=', 'incidents.ong_id')
+        .limit(5)
+        .offset((page - 1) * 5)
+        .select([
+            'incidents.*',
+            'ongs.nome',
+            'ongs.email',
+            'ongs.whatsapp',
+            'ongs.city',
+            'ongs.uf'
+        ]);
+
         res.header('X-Total-Count',count['count(*)']);
         return res.json(incidents);
     },
--- backend/src/index.js
+++ backend/src/index.js
@@ -37,4 +37,4 @@ app.use(express.json()); //configura para receber jsons em request
 app.use(routes);
 
 
-app.listen(3333);
+app.listen(process.env.PORT||3333);
